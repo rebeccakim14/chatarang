ed5c89d3f99defdfdff78b131f9f6abd922090d0
Rebecca Kim <rebeccakim@rebeccas-mbp.coldspring.marian.edu>
2018-07-03 16:12:14 -0400

    reset the message body after submit

--- src/MessageForm.js
+++ src/MessageForm.js
@@ -9,6 +9,7 @@ class MessageForm extends Component {
     handleSubmit = (ev) => {
         ev.preventDefault()
         this.props.addMessage(this.state.body)
+        this.setState({body: ''})
     }
 
     handleChange = (ev) => {
