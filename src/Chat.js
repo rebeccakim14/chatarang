81cba5e828156c0250a93f03d4cc3608e3e6ca32
Rebecca Kim <rebeccakim@rebeccas-mbp.coldspring.marian.edu>
2018-07-03 15:31:32 -0400

    add messages to Chat state

--- src/Chat.js
+++ src/Chat.js
@@ -5,11 +5,39 @@ import MessageList from './MessageList'
 import MessageForm from './MessageForm'
 
 class Chat extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      messages: [
+        { 
+          id: 1,
+          user: {
+              uid: 'randomString',
+              displayName: 'Rebecca',
+              email: 'rkim7987@gmail.com',
+          },
+          body: 'chat chat chat',
+         },
+
+         {
+           id: 2,
+           user: {
+               uid: 'anotherRandomString',
+               displayName: 'kim',
+               email: 'yeet@gmail.com', 
+           },
+           body: 'this dude is cool',
+         },
+    ]
+    }
+  }
+
   render() {
     return (
       <div className="Chat">
         <ChatHeader />
-        <MessageList />
+        <MessageList  messages={this.state.messages} />
         <MessageForm />
       </div>
     )
--- src/MessageList.js
+++ src/MessageList.js
@@ -3,32 +3,10 @@ import React from 'react'
 import Message from './Message'
 
 
-const MessageList = () => {
-    const messages = [
-        { 
-          id: 1,
-          user: {
-              uid: 'randomString',
-              displayName: 'Rebecca',
-              email: 'rkim7987@gmail.com',
-          },
-          body: 'chat chat chat',
-         },
-
-         {
-           id: 2,
-           user: {
-               uid: 'anotherRandomString',
-               displayName: 'kim',
-               email: 'yeet@gmail.com', 
-           },
-           body: 'this dude is cool',
-         },
-    ]
-
+const MessageList = (props) => {
     return (
         <div className='MessageList'>
-            { messages.map(msg => (
+            { props.messages.map(msg => (
                     <Message message={msg} key={msg.id}/>
                 ))
             }
