eba1500b699b37e43e4f2757cc8094af9c3888a1
Rebecca Kim <rebeccakim@rebeccas-mbp.coldspring.marian.edu>
2018-07-05 11:09:17 -0400

    added styles to MessageForm

--- src/MessageForm.js
+++ src/MessageForm.js
@@ -39,4 +39,48 @@ class MessageForm extends Component {
   }
 }
 
+const styles = {
+  messageForm: {
+    backgroundColor: 'white',
+    height: '3rem',
+    display: 'flex',
+    alignItems: 'stretch',
+    border: '2px solid #999',
+    borderRadius: '0.5rem',
+    margin: '0.25rem',
+    padding: 0,
+  },
+
+  icon: {
+    display: 'flex',
+    borderRadius: '0.5rem',
+    alignItems: 'center',
+    backgroundColor: 'white',
+    color: '#ccc',
+    padding: '0 0.5rem',
+    fontSize: '1.2rem',
+  },
+
+  input: {
+    flex: 1,
+    fontSize: '1.2rem',
+    border: 0,
+
+    ':focus': {
+      outline: 0,
+    },
+  },
+
+  button: {
+    fontSize: '1.5rem',
+    backgroundColor: '#1A8FE3',
+    color: 'white',
+    paddingLeft: '1rem',
+    paddingRight: '1rem',
+    borderTopRightRadius: '0.5rem',
+    borderBottomRightRadius: '0.5rem',
+    border: '1px solid white',
+  }
+}
+
 export default MessageForm
