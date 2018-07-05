dc818a3c537cb70f4a395fce7ecd05366c55e313
Rebecca Kim <rebeccakim@rebeccas-mbp.coldspring.marian.edu>
2018-07-05 15:53:13 -0400

    remove hardcoded messages

--- src/Chat.js
+++ src/Chat.js
@@ -9,29 +9,9 @@ class Chat extends Component {
     super()
 
     this.state = {
-      messages: [
-        {
-          id: 1,
-          user: {
-            uid: 'sdfs34849327',
-            displayName: 'Davey',
-            email: 'davey@fretless.com',
-          },
-          body: 'Chatting up a storm, yo!',
-        },
-
-        {
-          id: 2,
-          user: {
-            uid: 'sdlfkj35948',
-            displayName: 'Dana',
-            email: 'dana@fretless.com',
-          },
-          body: 'This guy is so annoying. I hate my job.',
-        },
-      ],
-    }
+      messages: [],
   }
+}
 
   addMessage = (body) => {
     const messages = [...this.state.messages]
